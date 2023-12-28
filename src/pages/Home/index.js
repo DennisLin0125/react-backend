877c78e41f1d7cda2871c4d8a0f3947ce3df6701
Dennis <740125aa@gmail.com>
2023-12-28 19:57:38 +0800

    Echarts圖 完成

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,43 @@
-const Home = () => {
-  return <div>Home</div>
-}
+import * as echarts from 'echarts';
+import { useEffect, useRef } from "react";
 
-export default Home
+const Home = () => {
+  // 將 useRef 移至組件主體內
+  const chartDom = useRef();
+  
+  useEffect(() => {
+    // 在此處使用 chartDom.current
+    const myChart = echarts.init(chartDom.current);
+    
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: ['Vue', 'React', 'Angular']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [10, 40, 70],
+          type: 'bar'
+        }
+      ]
+    };
+    
+    myChart.setOption(option);
+    
+    // 在組件卸載時銷毀圖表
+    return () => {
+      myChart.dispose();
+    };
+  }, []);
+  
+  return (
+    <div>
+      <div ref={chartDom} style={{width: '500px', height: '400px'}}></div>
+    </div>
+  );
+};
+
+export default Home;
