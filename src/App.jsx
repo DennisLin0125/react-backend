091cd27091f0dc91bd18d232699c509196d1f260
gen_lin <gen_lin@goodwill.com.tw>
2024-01-03 13:14:44 +0800

    :fire: chore: test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Button } from 'antd';
 function App() {
   return (
     <div className="App">
-      <Button type="primary">Button</Button>
+      <Button type="primary">Buttontest</Button>
     </div>
   );
 }
